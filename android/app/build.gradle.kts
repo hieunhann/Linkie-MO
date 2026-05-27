ff19a2b0291cf1908f9cb92d8a52b4f888a84873
hieunhan <tahieunhann@gmail.com>
2026-05-27 14:31:30 +0700

    chore: automate Android Release app signing in Gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -25,17 +25,24 @@ android {
         applicationId = "com.linkie.linkie_mo"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("upload-keystore.jks")
+            storePassword = "linkie123456"
+            keyAlias = "upload"
+            keyPassword = "linkie123456"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
